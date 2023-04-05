b3f663aec517fe898743505b96d498d30b98d4b3
Staninna <26458805+Staninna@users.noreply.github.com>
2023-04-05 16:06:58 +0200

    Added basic logging service

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
+#![allow(dead_code, warnings, unused_imports, unused_variables)]
 use futures::future::{ready, BoxFuture, Ready};
 use hyper::service::make_service_fn;
 use hyper::{Body, Request, Response, Server};
 use std::convert::Infallible;
 use std::net::SocketAddr;
 use std::task::{Context, Poll};
+use std::time::Instant;
 use tower::Service;
+
+// Function service ------------------------------------------------
 
 // async fn handle(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
 //     Ok(Response::new(Body::from("Hello World")))
 // }
+
+// Main -----------------------------------------------------------
 
 #[tokio::main]
 async fn main() {
@@ -21,6 +27,7 @@ async fn main() {
     // And a MakeService to handle each connection
     let make_service = make_service_fn(|_conn| async {
         let svc = HelloWorld;
+        let svc = LoggingService::new(svc);
         Ok::<_, Infallible>(svc)
     });
 
@@ -34,7 +41,10 @@ async fn main() {
     }
 }
 
+// Hello World Service ---------------------------------------------
+
 // Simple struct that is an Service that response over HTTP with Hello, World!
+#[derive(Clone, Copy)]
 struct HelloWorld;
 
 // Implement the service trait with the request type an HTTP request
@@ -47,10 +57,11 @@ impl Service<Request<Body>> for HelloWorld {
 
     // It is instant ready (must be an future)
     // Ready is an Future that isnt an future but is (makes sense?)
-    // type Future = Ready<Result<Self::Response, Self::Error>>;
+    type Future = Ready<Result<Self::Response, Self::Error>>;
 
     // BoxeFuture
-    type Future = BoxFuture<'static, Result<Self::Response, Self::Error>>;
+    // Boxed future is an future that is boxed this comes with an performance cost but is more flexible
+    // type Future = BoxFuture<'static, Result<Self::Response, Self::Error>>;
 
     // Ask serves OK to reply (this cause always yes)
     fn poll_ready(&mut self, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
@@ -59,10 +70,85 @@ impl Service<Request<Body>> for HelloWorld {
 
     // Respond to aqual request
     fn call(&mut self, _req: Request<Body>) -> Self::Future {
-        // Construct an Ready future
-        // ready(Ok(Response::new(Body::from("Hello, World!"))))
+        // Wait a few seconds
+        let mut idx = 0_u64;
+        loop {
+            if idx == 25000000000 {
+                break;
+            } else {
+                idx += 1
+            }
+        }
 
+        // Ready future
+        // Construct an Ready future
+        ready(Ok(Response::new(Body::from("Hello, World!"))))
+
+        // Boxed future
         // Construct an Boxed future
-        Box::pin(async { Ok(Response::new(Body::from("Hello, World!"))) })
+        // Box::pin(async { Ok(Response::new(Body::from("Hello, World!"))) })
     }
 }
+
+// Logging Service ------------------------------------------------
+
+// Simple struct that looks at the request and response and logs them
+#[derive(Clone, Copy)]
+struct LoggingService<S> {
+    inner: S,
+}
+
+impl<S> LoggingService<S> {
+    fn new(inner: S) -> Self {
+        Self { inner }
+    }
+}
+
+impl<S, B> Service<Request<B>> for LoggingService<S>
+where
+    S: Service<Request<B>> + Clone + Send + 'static,
+    B: Send + 'static,
+    S::Future: Send,
+{
+    type Response = S::Response; // Same as S: Service this case inner
+    type Error = S::Error; // Same as S: Service this case inner
+
+    // type Future = S::Future;
+
+    // BoxeFuture
+    // Boxed future is an future that is boxed this comes with an performance cost but is more flexible
+    type Future = BoxFuture<'static, Result<Self::Response, Self::Error>>;
+
+    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.inner.poll_ready(cx)
+    }
+
+    fn call(&mut self, req: Request<B>) -> Self::Future {
+        // Get info for logging
+        let method = req.method().to_owned();
+        let path = req.uri().path().to_string();
+
+        // Clone inner
+        let mut inner = self.inner.clone();
+
+        Box::pin(async move {
+            // Log the reqest
+            log::info!("request  {} {}", method, path);
+
+            // Starts an timer to time request
+            let start = Instant::now();
+
+            // Get an response
+            let res = inner.call(req).await;
+
+            // Log the response
+            let time_spend = start.elapsed();
+            log::info!("response {} {} time={:?}", method, path, time_spend);
+
+            // Return the response
+            res
+        })
+    }
+}
+
+// TODO: https://youtu.be/16sU1q8OeeI?t=2906
